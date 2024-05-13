cab906104af5f68ec75de5c811429c0cafb10e5d
ksws0134422 <46697280+ksws0134422@users.noreply.github.com>
2024-05-13 11:38:49 +0800

    Update itv.py

--- itv.py
+++ itv.py
@@ -22,8 +22,11 @@ def modify_urls(url):
     ip_address = url[ip_start_index:ip_end_index]
     port = url[ip_end_index:]
     ip_end = "/iptv/live/1000.json?key=txiptv"
-    modified_url = f"{base_url}{ip_address}{port}{ip_end}"
-    modified_urls.append(modified_url)    
+    for i in range(1, 256):
+        modified_ip = f"{ip_address[:-1]}{i}"
+        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
+        modified_urls.append(modified_url)
+
     return modified_urls
 
 def is_url_accessible(url):
